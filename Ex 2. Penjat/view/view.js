27eb6a87378d278c399e5d1a708f290a274b3400
GuidoFC <guidofigueroa96@gmail.com>
2024-10-07 17:35:28 +0200

    Ya hemos colocado la imagen

--- Ex 2. Penjat/view/view.js
+++ Ex 2. Penjat/view/view.js
@@ -5,3 +5,21 @@ function createDiv() {
 
     getBody.appendChild(divNew);
   }
+  // create tag img
+  // only i need one tag img, later i will change this img with src
+function createImg(){
+    const getBody = document.querySelector("body");
+    const imgNew = document.createElement("img");
+    imgNew.setAttribute("id", "photo");
+    getBody.appendChild(imgNew);
+}
+
+// we put first photo and we make this funtion reusable
+function putImg(numPhoto) {
+    const getImg = document.querySelector("#photo");
+    getImg.src = "./assests/img/" + numPhoto + ".PNG";
+    // la imagen no es text-align
+    getImg.style.display = "block";
+    getImg.style.margin = "0 auto";
+    
+  }
