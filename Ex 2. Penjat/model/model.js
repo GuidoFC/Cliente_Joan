af427e2497e2b820f2f45f2fd96d3405637849d9
GuidoFC <guidofigueroa96@gmail.com>
2024-10-07 18:31:29 +0200

    Ya he creado los botones con todas las letras!

--- Ex 2. Penjat/model/model.js
+++ Ex 2. Penjat/model/model.js
@@ -1,7 +1,7 @@
 function Letter(letter) {
     // Si quieres asegurarte de que los parámetros sean siempre strings,
     // puedes convertirlos explícitamente dentro del constructor:
-    this.letter = String(letter); // Convertir a string
+    this.letraAlfrabeto = String(letter); // Convertir a string
     this.seleccionada = false; 
     this.color = "gray";
     
