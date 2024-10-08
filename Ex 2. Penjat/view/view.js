0d04d8ec5665a06d2ac00c0b7de1dac35efbbdf6
GuidoFC <guidofigueroa96@gmail.com>
2024-10-08 20:22:34 +0200

    refactorizacion

--- Ex 2. Penjat/view/view.js
+++ Ex 2. Penjat/view/view.js
@@ -58,4 +58,32 @@ function putImg(numPhoto) {
       "rgb(101, 10, 101)";
   }
 
+  function createWordsToGuess(getDivContendor) {
+    const arrayWords = ["Horno", "Mundo", "Classe", "Ordenador"];
+    const newParagraph = document.createElement("p");
+    newParagraph.setAttribute("id","palabaElegida");
+    // Estilo
+    let palabraAdivinar = arrayWords[3];
+    newParagraph.textContent = arrayWords[3];
+    newParagraph.style.textAlign = "center";
+    newParagraph.style.fontSize = "35px";
+    // add to our html
+    const getDivApp = document.querySelector("#app");
+    getDivApp.appendChild(newParagraph);
+    return palabraAdivinar;
+  }
+
+  function transformWordsToUnderscore(palabraAdivinar) {
+    // funtion to count how many letter have this word
+    // Variable String have the method length
+    // debugger;
+    // console.log(palabraAdivinar.length);
+    let allLettersAreUnderscore = "";
+    for (let i = 0; i < palabraAdivinar.length; i++) {
+      allLettersAreUnderscore = allLettersAreUnderscore + "_ ";
+    }
+    const newParagraph = document.querySelector("#palabaElegida");
+    newParagraph.textContent = allLettersAreUnderscore;
+  }
+
   
